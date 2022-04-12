95b28aea06187e7c91c3a6449f9c008016967b38
DannSoy <103258301+DannSoy@users.noreply.github.com>
2022-04-12 20:32:34 +0300

    Base converter - update v3.0
    The base converter is done except for the case when someone tries to input a number that doesn't exist in the base they specified.
    
    Co-Authored-By: Zenaticon <21153869+Zenaticon@users.noreply.github.com>

--- App.cpp
+++ App.cpp
@@ -2,10 +2,21 @@
 #include <string>
 #include <math.h>
 using namespace std;
-int nr_1=0, nr_2=0, i=0, j=0, v[50];
+int h = 0, v[50];
 char text_v[50];
-string text_nr_1, text_nr_2;
 
+class Base
+{
+    public:
+        int nr = 0;
+        string text;
+        int base_1;
+        int base_2;
+        void read_base(string message, int number);
+        void read_nr();
+        void converter();
+        void calculator();
+};
 void help(){
     cout<<endl<<"For more information on a specific command, type 'help_<command_name>'"<<endl<<endl;
     cout<<"converter    Converts a number's base to another base and displays it."<<endl;
@@ -14,7 +25,48 @@ void help(){
     cout<<"For more information on a specific command, type 'help_<command_name>'"<<endl<<endl;
 }
 
-int get_16(char nr){
+bool check_number(string str, int base, bool state)
+{
+    for (int i = 0; i < str.length(); i++){
+        if(state){
+            if (base != 2){
+                if (isdigit(str[i]) == false)
+                    return false;
+            }else{
+                if (!((isdigit(str[i]) == true) && (str[i] == '1' || str[i] == '0')))
+                    return false;
+            }
+        }else{
+            if (!((isdigit(str[i]) == true)||(isalpha(str[i]) == true)))
+                return false;
+        }
+    }
+    
+    return true;
+}
+int app(){
+    Base nr1, nr2;
+    string text;
+    cout<<"Type a command: "<<endl;
+    //cin>>text;
+    text = "converter";
+    if(text == "help"){
+        help();
+    }else if(text == "converter"){
+        nr1.converter();
+    }else if(text == "calculator"){
+        cout<<endl<<"-calculator"<<endl;
+    }else if(text == "help_converter"){
+        cout<<endl<<"-help converter"<<endl;
+    }else if(text == "help_calculator"){
+        cout<<endl<<"-help calculator"<<endl;
+    }else{
+        cout<<"Unknown command, type 'help' to see more about the program"<<endl<<endl;
+    }
+    app();
+}
+
+int get_36(char nr){
     switch(nr) {
         case 'A':
             return 10;
@@ -34,101 +86,268 @@ int get_16(char nr){
         case 'F':
             return 15;
             break;
+        case 'G':
+            return 16;
+            break;
+        case 'H':
+            return 17;
+            break;
+        case 'I':
+            return 18;
+            break;
+        case 'J':
+            return 19;
+            break;
+        case 'K':
+            return 20;
+            break;
+        case 'L':
+            return 21;
+            break;
+        case 'M':
+            return 22;
+            break;
+        case 'N':
+            return 23;
+            break;
+        case 'O':
+            return 24;
+            break;
+        case 'P':
+            return 25;
+            break;
+        case 'Q':
+            return 26;
+            break;
+        case 'R':
+            return 27;
+            break;
+        case 'S':
+            return 28;
+            break;
+        case 'T':
+            return 39;
+            break;
+        case 'U':
+            return 30;
+            break;
+        case 'V':
+            return 31;
+            break;
+        case 'W':
+            return 32;
+            break;
+        case 'X':
+            return 33;
+            break;
+        case 'Y':
+            return 34;
+            break;
+        case 'Z':
+            return 35;
+            break;
         default:
-            int x=nr;
-            return x-48;
+            int x = nr;
+            return x - 48;
             break;
     }
 }
 
-void read_nr(int base_1){
-    cout<<"Insert the number: ";
-    i=0;
-    if(base_1 > 15){
-        // while(*(text_nr_1 + i) != '\n'){
-        //     cin>>*(text_nr_1 + i);
-        //     cout<<endl<<i<<" "<<*(text_nr_1 + i);
-        //     i++;
-        // }
-        cin>>text_nr_1;
+void Base::read_base(string message, int number){
+    string base_text;
+    if(number == 1){
+        cout<<"Insert base to convert: ";
+    }else if(number==2){
+        cout<<"Insert base to convert to: ";
+    }
+    cin >> base_text;
+    if (check_number(base_text, 10, true)){
+        if(number == 1){
+            base_1 = stoi(base_text);
+        }else if(number==2){
+            base_2 = stoi(base_text);
+        }
     }else{
-        cin>>nr_1;
+        cout << "The base is incorrect!" << endl << "Please try again" << endl;
+        read_base(message,number);
     }
 }
 
-int multiply(int nr, int base_2, string text_nr){
-    int k=nr.length()-1;
-    for(i=0;i<nr.length();i++){
-        text_v[k]=text_nr[i];
+void Base::read_nr(){
+    cout<<"Insert the number: ";
+    cin >> text;
+    if (base_1>10)
+    {
+        if(!check_number(text,base_1,false)){
+            cout << "The number is incorrect!" << endl << "Please try again" << endl;
+            read_nr();
+        }
+    }
+    else
+    {
+         if(check_number(text,base_1,true)){
+             nr = stoi(text);
+         }else{
+            cout << "The number is incorrect!" << endl << "Please try again" << endl;
+            read_nr();
+         }
+    }
+}
+
+int multiply(string text, int base){
+    int k = text.length() - 1, nr=0;
+    char text_v[50] = {0};
+    for (int i = 0; i < text.length(); i++)
+    {
+        text_v[k] = text[i];
         k--;
     }
-    nr=0;
-    for(int j=0;j<text_nr.length();j++){
-        nr = nr + get_16(text_v[j])*pow(base_2,j);
-    }
-    //afisare rezultat
-    cout<<"\nRezultatul conversiei in baza 10 este: "<<nr;
-}
-
-int divide(int nr,int base){
-    int aux=0,k=0;
-    while(nr != 0){
-        aux = aux * 10 + nr%base;
-        nr /= base;
-        k++;
-    }
-    nr = 0;
-    while(k != 0){
-        nr = nr * pow(10,k) + aux%10;
-        aux /= 10;
-        k++;
+    for (int j = 0; j < text.length(); j++)
+    {
+        nr = nr + get_36(text_v[j]) * pow(base, j);
     }
     return nr;
 }
 
-int converter(){
-    int base_1,base_2,nr;
-    cout<<"Insert base to convert: "; cin>>base_1;
-    cout<<"Insert base to convert to: "; cin>>base_2;
-    read_nr(base_1);
-    cout<<"Result: ";
-    // for(char *k=text_nr_1;k<text_nr_1 + i;k++){
-    //     cout<<*(k);
-    // }
-    if(base_1 > 15){
-        //se converteste char-ul in baza 10
-        nr = get_16(text_v);
+string divide(int nr, int base){
+    string text="";
+    h = 0;
+    while(nr!=0){
+        v[h]=nr%base;
+        nr=nr/base;
+        h++;
     }
-
-    if(base_1 == base_2){
-        cout<<nr;
-    }else if(base_1 == 10){
-        cout<<divide(nr,base_2);
-    }else if((base_1 != 10)&&(base_2 == 10)){
-        cout<<multiply(nr,base_2);
-    }else{
-        cout<<divide(multiply(nr,base_1),base_2);
+    for(int j=h-1;j>=0;j--){
+        if(base>10){
+            switch(v[j]) {
+                case 10:
+                    text.append("A");
+                    break;
+                case 11:
+                    text.append("B");
+                    break;
+                case 12:
+                    text.append("C");
+                    break;
+                case 13:
+                    text.append("D");
+                    break;
+                case 14:
+                    text.append("E");
+                    break;
+                case 15:
+                    text.append("F");
+                    break;
+                case 16:
+                    text.append("G");
+                    break;
+                case 17:
+                    text.append("H");
+                    break;
+                case 18:
+                    text.append("I");
+                    break;
+                case 19:
+                    text.append("J");
+                    break;
+                case 20:
+                    text.append("K");
+                    break;
+                case 21:
+                    text.append("L");
+                    break;
+                case 22:
+                    text.append("M");
+                    break;
+                case 23:
+                    text.append("N");
+                    break;
+                case 24:
+                    text.append("O");
+                    break;
+                case 25:
+                    text.append("P");
+                    break;
+                case 26:
+                    text.append("Q");
+                    break;
+                case 27:
+                    text.append("R");
+                    break;
+                case 28:
+                    text.append("S");
+                    break;
+                case 29:
+                    text.append("T");
+                    break;
+                case 30:
+                    text.append("U");
+                    break;
+                case 31:
+                    text.append("V");
+                    break;
+                case 32:
+                    text.append("W");
+                    break;
+                case 33:
+                    text.append("X");
+                    break;
+                case 34:
+                    text.append("Y");
+                    break;
+                case 35:
+                    text.append("Z");
+                    break;
+                default:
+                    text.append(to_string(v[j]));
+                    break;
+            }
+        }else{
+            text.append(to_string(v[j]));
+        }
     }
+    return text;
 }
 
-int app(){
-    string text;
-    cout<<"Type a command: ";
-    cin>>text;
-    if(text == "help"){
-        help();
-    }else if(text == "converter"){
-        converter();
-    }else if(text == "calculator"){
-        cout<<endl<<"-calculator"<<endl;
-    }else if(text == "help_converter"){
-        cout<<endl<<"-help converter"<<endl;
-    }else if(text == "help_calculator"){
-        cout<<endl<<"-help calculator"<<endl;
-    }else{
-        cout<<"Unknown command, type 'help' to see more about the program"<<endl<<endl;
+void Base::converter(){
+    read_base("Insert base to convert: ",1);
+    read_base("Insert base to convert to: ",2);
+    read_nr();
+    cout << "Result: ";
+    if (base_1 < 2 || base_1 > 36 || base_2 < 2 || base_2 > 36)
+    {
+        cout << "At least one number is incorrect for one of the bases" << endl << endl;
+        app();
+    }else if(base_1 == base_2){
+        if(base_1>10){
+            cout << text<<endl;
+        }else{
+            cout << nr<<endl;
+        }
     }
-    app();
+    else if (base_1 > 10)
+    {
+        if(base_2 == 10){
+            nr = multiply(text,base_1);
+            cout << nr << endl;
+        }else{
+            text = divide(multiply(text, base_1), base_2);
+            cout << text << endl;
+        }
+    }
+    else if (base_1 == 10)
+    {
+        text = divide(nr, base_2);
+        cout << text << endl;
+    }else if(base_1<10){
+        if(base_2 == 10){
+            nr = multiply(text,base_1);
+            cout << nr << endl;
+        }else{
+            text = divide(multiply(text, base_1), base_2);
+            cout << text << endl;
+        }
+    }
 }
 
 int main(){
